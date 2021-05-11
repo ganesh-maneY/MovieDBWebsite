0bcbd3fe373cbe3fde43d99da115faca0d3e8924
Ganesh <ganesh@fokcus.com>
2021-05-11 14:59:51 -0700

    Changed Avatar Component

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,14 +2,14 @@ const Avatar = ({ cast }) => {
   return (
     <>
       <div className="flex flex-col  object-contain  rounded shadow-lg m-2 border-2 transition duration-100 transform hover:scale-105 ">
-        <div className="inline-block w-64 h-auto max-w-xs">
+        <div className="inline-block w-48 h-auto max-w-xs">
           <img
-            className="object-center  mx-auto border-2 "
+            className="object-center  mx-auto border-b-2 "
             src={cast.img}
             alt={cast.title}
           />
           <div className="px-6 py-4 ">
-            <div className="w-full text-left font-bold text-xl ">
+            <div className="w-full text-left font-bold text-lg md:text-xl ">
               <p>{cast.name}</p>
             </div>
             <div className="pb-2">
